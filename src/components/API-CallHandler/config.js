13d0d5110425e1c7ab3e53b2c5602c16a5261a12
Deshabhakt <dgavali1@gmail.com>
2022-07-22 01:38:30 +0530

    production build ready with all error handling

--- src/components/API-CallHandler/config.js
+++ src/components/API-CallHandler/config.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = 'http://192.168.131.13:5000/'
+export const SERVER_URL = 'https://task-manager-101.herokuapp.com/'
 
 export const HEADERS = (token) => {
 	const headers = {
