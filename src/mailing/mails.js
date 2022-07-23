bd3c9739f8ff04e6810f14bda0d5ccbbfd42731b
Deshabhakt <dgavali1@gmail.com>
2022-07-23 23:47:49 +0530

    modified mailing content

--- src/mailing/mails.js
+++ src/mailing/mails.js
@@ -2,15 +2,15 @@ const verificationMail = (name, verificationLink) => {
 	return {
 		subject: 'Task!t | Email verification required',
 		body: `<div>
-				<h1>Hey ${name}, Welcome to ToDo List</h1>
-				<p>
+				<h1>Hey ${name}, Welcome to Task!t</h1>
+				<h2>
 					We are happy to have you onboard. <br />
 					Just one more thing to do...
 					<br />
 					click on below link to verify your email-address.
-				</p>
-				<a href=${verificationLink}>click here</a>
-				<h2>Regards,<br/>Team Task!t</h2>
+				</h2>
+				<h3 href=${verificationLink}>click here</h3>
+				<h3>Regards,<br/>Team Task!t</h3>
 			</div>`,
 	}
 }
@@ -20,8 +20,8 @@ const verificationSuccessMail = (name) => {
 		subject: 'Task!t | Email verified Successfully',
 		body: `<div>
 				<h1>Hey ${name},</h1>
-				<p>Your Mail has been verified successfully.</p>
-				<h2>Regards,<br/>Team Task!t</h2>
+				<h3>Your Mail has been verified successfully.</h3>
+				<h3>Regards,<br/>Team Task!t</h3>
 			</div>`,
 	}
 }
@@ -31,9 +31,9 @@ const resetPasswordVerificationMail = (name, verificationLink) => {
 		subject: 'Task!t | Reset Password',
 		body: `<div>
 				<h1>Hey ${name},</h1>
-				<p>To reset password click on below link</p>
-				<a href=${verificationLink}>click here</a>
-				<h2>Regards,<br/>Team Task!t</h2>
+				<h2>To reset password click on below link</h2>
+				<h3 href=${verificationLink}>click here</h3>
+				<h3>Regards,<br/>Team Task!t</h3>
 			</div>`,
 	}
 }
@@ -43,10 +43,11 @@ const passwordChangeMail = (name, loginLink) => {
 		subject: 'Task!t | Password reset successfully',
 		body: `<div>
 				<h1>Hey ${name},</h1>
-				<p>Your password has been reset successfully.<br/>login using below link
-				</p>
-				<a href=${loginLink}>click here</a>
-				<h2>Regards,<br/>Team Task!t</h2>
+				<h2>Your password has been reset successfully.<br/>login using below link
+				</h2>
+				<link>${loginLink}</link>
+				<h3 href=${loginLink}>click here</h3>
+				<h3>Regards,<br/>Team Task!t</h3>
 			</div>`,
 	}
 }
