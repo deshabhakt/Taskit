a2e86f39e90ffd08c1d17fa9532e6c1ef29329b7
Deshabhakt <dgavali1@gmail.com>
2022-07-25 13:27:48 +0530

    added onsubmit handler to contact form

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -32,15 +32,27 @@ function Contact() {
 
 	return (
 		<div className="main-content contact__main-div">
-			<p className='contact__description'>Anything to add or comment?<br /> Do reach out by filling feedback form and I will get back to you as soon as possible</p>
-			<form className="contact__form">
+			<p className="contact__description">
+				Anything to add or comment?
+				<br /> Do reach out by filling feedback form and I will get back
+				to you as soon as possible
+			</p>
+			<form className="contact__form" onSubmit={formSubmitHandler}>
 				<h1>Feedback Form</h1>
 				<Card className={'contact__card'}>
-					<label htmlFor='name'>Name</label>
-					<input type="text" name="name" onChange={formInputChangeHandler} />
-					<label htmlFor='email'>Email</label>
-					<input type="email" name="email" onChange={formInputChangeHandler} />
-					<label htmlFor='message'>Message</label>
+					<label htmlFor="name">Name</label>
+					<input
+						type="text"
+						name="name"
+						onChange={formInputChangeHandler}
+					/>
+					<label htmlFor="email">Email</label>
+					<input
+						type="email"
+						name="email"
+						onChange={formInputChangeHandler}
+					/>
+					<label htmlFor="message">Message</label>
 					<textarea
 						name="message"
 						type="text"
@@ -48,22 +60,20 @@ function Contact() {
 						cols={30}
 						onChange={formInputChangeHandler}
 					/>
-					<div className='contact__submit-button-div'>
-						<Button type={'submit'} className='btn btn-success'>Submit</Button>
+					<div className="contact__submit-button-div">
+						<Button type={'submit'} className="btn btn-success">
+							Submit
+						</Button>
 					</div>
 				</Card>
 			</form>
-			{
-				feedBack.h1.length > 0 &&
+			{feedBack.h1.length > 0 && (
 				<FormFeedback
 					feedBack={feedBack}
-					h1Style={
-						{ color: 'green' }
-					}
+					h1Style={{ color: 'green' }}
 					pStyle={{ color: 'gray' }}
 				/>
-			}
-
+			)}
 		</div>
 	)
 }
